95fac9f9f57c069470933920e93dcba28c51686e
Kaioz-oh <hoangmai.tq2000@gmail.com>
2020-10-31 23:42:43 +0700

    add rle and tfidf1

---
+++ Ex7/RLE.py
@@ -0,0 +1,52 @@
+t = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2]
+ct = [0, 5, 1, 1, 0, 5, 2, 1]
+
+def length(t):
+    if len(t) > 0:
+        count = 2
+
+        for i in range(len(t) - 1):
+            if t[i] != t[i + 1]:
+                count += 2
+        
+        return count
+    else:
+        return 0
+
+# print(length(t))
+    
+def compress(t):
+    ct = []
+    temp = dict()
+    
+    b = t.copy()
+
+    for i in range(len(t)):
+        temp.update({i: t[i]})
+
+
+
+    return ct
+
+print(compress(t))
+
+def lengthInverse(ct):
+    length = 0
+
+    for i in range(len(ct)):
+        if i % 2 != 0:
+            length += ct[i]
+    return length
+    
+def decompress(ct):
+    result = []
+
+    for i in range(0, len(ct)):
+        if i % 2 != 0:
+            for j in range(ct[i]):
+                result.append(ct[i - 1])
+    
+    return result
+ct = [0, 5, 1, 1, 0, 5]
+# print(lengthInverse(ct))
+# print(decompress(ct))
---
+++ Ex7/TFIDF1.py
@@ -0,0 +1,182 @@
+import os
+import math
+
+class Doc:
+    '''
+    Lớp mô tả một đối tượng văn bản, lớp này có 1 thuộc tính là wordCount kiểu từ điển
+    
+    với key là các từ trong văn bản và value là số lần các từ đó xuất hiện trong văn bản
+    
+    CHÚ Ý: Để cho đơn giản, mỗi từ trong văn bản được phân cách bởi dấu cách, các từ phân biệt hoa thường...
+    
+    Nói chung 2 từ A và B mà A == B nhận giá trị false là khác nhau. 
+    Ví dụ từ "anh" và "Anh" là khác nhau
+          từ "anh." và "anh" là khác nhau
+    
+    '''
+    def __init__(self,filename):
+        self.wordCount = dict()
+        
+        self.loadWordCountFromFile(filename)
+        
+    def loadWordCountFromFile(self,filename):
+        '''
+        Hàm thực hiện đọc vào file filename và đếm tần suất xuất hiện các từ rồi đưa vào wordCount
+        
+        Hàm này được gọi đến trong hàm dựng, do vậy bắt buộc phải viết đúng hàm này, nếu không các hàm khác sẽ không được
+        chấm điểm
+        
+        '''
+
+        file = open(filename, 'rt', encoding='utf-8') 
+        data = file.read().strip().split()
+        sets = set(data)
+
+        for i in sets:
+            count = data.count(i)
+            self.wordCount.update({i: count})
+        
+        return self.wordCount
+
+    
+def compers(x):
+    return x[1], x[0]
+
+class TFIDF:
+    '''
+        Lớp mô tả việc tính chỉ số TFIDF cho các từ trong văn bản dựa trên kho văn bản
+        Lớp này có 2 thuộc tính là corpusPath và data, trong đó:
+        - corpusPath là đường dẫn tới thư mục chứa các tệp văn bản
+        - data là một từ điển với key là tên các file văn bản, và value là các đối tượng Doc tương ứng
+        với key
+        
+    '''
+    def __init__(self, corpusPath):
+        
+        self.corpusPath = corpusPath
+        
+        self.data = dict()
+        
+        self.loadCorpus(corpusPath)
+        
+    
+    def loadCorpus(self,corpusPath):
+        '''
+        
+        Hàm thực hiện đọc các file văn bản trong thư mục corpusPath, và xây dựng các đối tượng
+        
+        Doc tương ứng và đưa vào data
+        
+         Hàm này được gọi đến trong hàm dựng, do vậy bắt buộc phải viết đúng hàm này, nếu không các hàm khác sẽ không được
+        chấm điểm
+        
+        '''
+
+        listPath = os.listdir(corpusPath)
+
+        for i in listPath:
+            path = corpusPath + '/' + i
+            doc = Doc(path)
+            self.data.update({i: doc})
+        
+        return self.data        
+    
+    def tf(self,w,d):
+        '''
+        Tính và trả về chỉ số tf của từ w trong Doc d (d là 1 đối tượng Doc)
+        Công thức tính tf(w,d) như sau:
+        - Gọi tfw là số lần w xuất hiện trong d (ví dụ tfw = 3)
+        - Gọi tfm là số lần 1 từ bất kỳ xuất hiện nhiều nhất trong d (ví dụ tfm = 6)
+        - khi đó tf(w,d) = tfw/tfm (ví dụ 3/6 = 0.5)
+        
+        '''
+        # doc = Doc(d)
+        data = d.wordCount
+
+        tfw = 0
+        tfm = max(data.values())
+
+        if w in data:
+            tfw = data[w]
+        # print(tfw, tfm)
+        return tfw / tfm
+    
+    def idf(self, w):
+        '''
+        Tính và trả chỉ số idf của từ w trong kho văn bản data
+        Chỉ số idf của w là idf(w) được tính như sau:
+        - Gọi N là số văn bản có trong kho data (ví dụ N = 10)
+        - Gọi m là số văn bản trong kho data có chứa từ w (ví dụ m = 3)
+        - khi đó: idf(w) = log(N/(1+m)) (ví dụ idf(w) = log(10/(1+3)) = 0.9162907318741551)
+        
+        CHÚ Ý DÙNG math.log để tính log
+        '''
+        
+        n = len(self.data)
+        m = 0
+        for i in self.data:
+            doc = self.data[i]
+            if w in doc.wordCount:
+                m += 1
+        
+        return math.log(n / (1 + m))
+    
+    def tfidf(self, w, d):
+        '''
+        Hàm tính và trả lại chỉ số tfidf của từ w trong văn bản d (d là một đối tượng Doc) trên kho dữ liệu data
+        Chỉ số tfidf(w,d) được tính như sau:
+        tfidf(w, d) = tf(w,d) * idf(w)
+        '''
+
+        tf = self.tf(w, d)
+        idf = self.idf(w)
+        
+        return tf * idf
+    
+    
+    def getKTopicWordFromCopus(self, k):
+        '''
+        Hàm thực hiện tính và trả lại danh sách k từ khác nhau có chỉ số tfidf cao nhất trong kho văn bản data
+        
+        Danh sách được sắp xếp tăng dần theo thứ tự từ điển mặc định (không cần sắp xếp theo tiếng Việt)
+        
+        CHÚ Ý: 2 từ giống nhau trong 2 văn bản khác nhau có thể cùng có chỉ số tfidf trong top k 
+        từ có chỉ số tfidf cao nhất, tuy nhiên kết quả chỉ lấy 1 từ 
+        (có nghĩa là trong danh sách kết quả, k từ đôi một khác nhau)
+        '''
+        
+        ks = []
+        temp = []
+
+        for i in self.data:
+            doc = self.data[i]
+            for j in doc.wordCount:
+                t = self.tfidf(j, doc)
+                tup = (j, t)
+                temp.append(tup)
+        # print(temp)
+        sets = set(temp)
+        # print('\n')
+        # print(sets)
+        tm = list(sets)
+        tm.sort(key=compers, reverse=True)
+
+        # print(tm)
+        
+        for i in range(k):
+            ks.append(tm[i][0])
+        
+        ks.sort()
+
+        return ks
+
+corpusPath = './Ex7/text/data'
+# test = Doc(filename)
+test2 = TFIDF(corpusPath)
+
+# print(test2.tf('A', test))
+# print(test.wordCount)
+
+# print(test2.data)
+
+print(test2.getKTopicWordFromCopus(7))
